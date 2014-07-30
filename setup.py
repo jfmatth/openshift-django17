a74807ee7038ba432f9959ec2203a79d0523d4a8
John Matthew <john@compunique.com>
2014-07-29 22:33:11 -0700

    remove django from setup.py's requirements.

--- setup.py
+++ setup.py
@@ -6,5 +6,4 @@ setup(name='Sample django 1.6 on Red Hat Openshift',
     author='Your Name',
     author_email='example@example.com',
     url='http://www.python.org/sigs/distutils-sig/',
-    install_requires=['Django>=1.6', 'Django<1.7'],
 )
