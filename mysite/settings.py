ca1dad716d200ac9257c3de35aacec9feb2960e6
John Matthew <john@compunique.com>
2014-12-24 07:20:35 -0800

    Update settings.py
    Forgot to mention the templates directory.  Thanks to Bill (kc1) for finding this one.

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,3 +128,9 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR,"static"),
 )
 
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'templates'),
+)
