994b78f50a678bda5a368d4ef434595b04d796b6
biwin <biwin007@gmail.com>
2015-05-20 20:42:27 +0530

    replaces hardcoded version display with django.get_version()

--- mysite/views.py
+++ mysite/views.py
@@ -1,7 +1,9 @@
 from django.views.generic import View
 from django.http import HttpResponse
+from django import get_version
+
 
 class Index(View):
 
     def get(self, request, *args, **kwargs):
-        return HttpResponse('django 1.7 on Openshift')
+        return HttpResponse(get_version())
